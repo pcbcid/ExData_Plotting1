4b2fc5a663a764a789725aa340ed3cbef562bb73
francisco <fran682003@gmail.com>
2015-07-09 11:01:50 +0200

    comit plot1.R y plot2.R

--- plot1.R
+++ plot1.R
@@ -4,7 +4,8 @@ sdata <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
 
 
 gAP <- as.numeric(sdata$Global_active_power)
-dev.copy(png, file="plot1.png", height=480, width=480)
 hist(gAP, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
 
+
+dev.copy(png, file="plot1.png", height=480, width=480)
 dev.off()
--- plot2.R
+++ plot2.R
@@ -3,10 +3,13 @@ data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec="
 sdata <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
 
 
+datetime <- strptime(paste(sdata$Date, sdata$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
 gAP <- as.numeric(sdata$Global_active_power)
-dev.copy(png, file="plot1.png", height=480, width=480)
-hist(gAP, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
 
+
+plot(datetime, gAP, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+
+dev.copy(png, file="plot2.png", height=480, width=480)
 dev.off()
 
 
